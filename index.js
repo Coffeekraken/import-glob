5d43c5f75f44edad1fb6ec243525309a09f321bf
Olivier Bossel <olivier.bossel@gmail.com>
2018-12-09 21:17:23 +0100

    add “require” support

--- index.js
+++ index.js
@@ -3,36 +3,37 @@ var path = require("path");
 
 module.exports = function(source) {
   this.cacheable();
-  var regex = /.?import + ?((\w+) +from )?([\'\"])(.*?)\3/gm;
+  var regex = /.?(import|require)\s?\(?((\w+) +from )?([\'\"])(.*?)\4/gm;
+  var regexRequire = /.+?require\([\'\"](.+)[\'\"]\)/gm
   var importModules = /import +(\w+) +from +([\'\"])(.*?)\2/gm;
   var importFiles = /import +([\'\"])(.*?)\1/gm;
   var requireFiles = /require\([\'\"](.*?)\1/gm;
   var importSass = /@import +([\'\"])(.*?)\1/gm;
   var resourceDir = path.dirname(this.resourcePath);
-  function replacer(match, fromStatement, obj, quote, filename) {
+  function replacer(match, statement, fromStatement, obj, quote, filename) {
     var modules = [];
     var withModules = false;
     if (!glob.hasMagic(filename)) return match;
-    var result = glob
-      .sync(filename, {
-        cwd: resourceDir
-      })
-      .map(function(file, index) {
-        var fileName = quote + file + quote;
-        if (match.match(importSass)) {
-          return '@import ' + fileName;
-        } else if (match.match(importModules)) {
-          var moduleName = obj + index;
-          modules.push(moduleName);
-          withModules = true;
-          return 'import * as ' + moduleName + ' from ' + fileName;
-        } else if (match.match(importFiles)) {
-          return 'import ' + fileName;
-        } else if (match.match(requireFiles)) {
-          return 'require(' + filename;
-        }
-      })
-      .join('; ');
+    var files = glob.sync(filename, {
+      cwd: resourceDir
+    });
+    var result = files.map(function(file, index) {
+      var fileName = quote + file + quote;
+      if (match.match(importSass)) {
+        return '@import ' + fileName;
+      } else if (match.match(importModules)) {
+        var moduleName = obj + index;
+        modules.push(moduleName);
+        withModules = true;
+        return 'import * as ' + moduleName + ' from ' + fileName;
+      } else if (match.match(importFiles)) {
+        return 'import ' + fileName;
+      } else if (match.match(requireFiles)) {
+        var endParenthese = (index < files.length-1) ? ')' : '';
+        return 'require(' + fileName + endParenthese;
+      }
+    })
+    .join('; ');
     if (result && withModules) {
       result += '; let ' + obj + ' = [' + modules.join(', ') + ']';
     }
